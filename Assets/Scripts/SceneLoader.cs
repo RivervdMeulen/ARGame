a9f079ae1afa62686d28805fe53717ef875d3792
RivervdMeulen <River@Rivers-MacBook-Pro.fritz.box>
2016-09-11 17:46:31 +0200

    Added ARToolKit and menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    
+    public void loadNewScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void loadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+	public void quitGame()
+	{
+		Application.Quit ();
+	}
+}
